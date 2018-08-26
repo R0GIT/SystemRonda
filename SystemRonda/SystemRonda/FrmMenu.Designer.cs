5c2aff77829be85ef558d385eb4d4b871d428da3
rrosas <rosasmr.svn@hotmail.com>
2018-08-26 16:12:33 -0500

    Listado de modulos en el menu

--- SystemRonda/SystemRonda/FrmMenu.Designer.cs
+++ SystemRonda/SystemRonda/FrmMenu.Designer.cs
@@ -28,20 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnModulo1 = new System.Windows.Forms.Button();
+            this.btnModulo2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSubmodulo13 = new System.Windows.Forms.Button();
+            this.btnSubmodulo12 = new System.Windows.Forms.Button();
+            this.btnSubmodulo11 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSubmodulo21 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnModulo1
+            // 
+            this.btnModulo1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnModulo1.FlatAppearance.BorderSize = 0;
+            this.btnModulo1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModulo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModulo1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnModulo1.Location = new System.Drawing.Point(0, 127);
+            this.btnModulo1.Name = "btnModulo1";
+            this.btnModulo1.Size = new System.Drawing.Size(127, 37);
+            this.btnModulo1.TabIndex = 0;
+            this.btnModulo1.Text = "Módulo 1";
+            this.btnModulo1.UseVisualStyleBackColor = false;
+            this.btnModulo1.Click += new System.EventHandler(this.btnModulo1_Click);
+            // 
+            // btnModulo2
+            // 
+            this.btnModulo2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnModulo2.FlatAppearance.BorderSize = 0;
+            this.btnModulo2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModulo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModulo2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnModulo2.Location = new System.Drawing.Point(0, 233);
+            this.btnModulo2.Name = "btnModulo2";
+            this.btnModulo2.Size = new System.Drawing.Size(127, 37);
+            this.btnModulo2.TabIndex = 1;
+            this.btnModulo2.Text = "Módulo 2";
+            this.btnModulo2.UseVisualStyleBackColor = false;
+            this.btnModulo2.Click += new System.EventHandler(this.btnModulo2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel1.Controls.Add(this.btnSubmodulo13);
+            this.panel1.Controls.Add(this.btnSubmodulo12);
+            this.panel1.Controls.Add(this.btnSubmodulo11);
+            this.panel1.Location = new System.Drawing.Point(0, 160);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(127, 73);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnSubmodulo13
+            // 
+            this.btnSubmodulo13.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnSubmodulo13.FlatAppearance.BorderSize = 0;
+            this.btnSubmodulo13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmodulo13.Location = new System.Drawing.Point(0, 47);
+            this.btnSubmodulo13.Name = "btnSubmodulo13";
+            this.btnSubmodulo13.Size = new System.Drawing.Size(127, 23);
+            this.btnSubmodulo13.TabIndex = 6;
+            this.btnSubmodulo13.Text = "Submodulo 3";
+            this.btnSubmodulo13.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmodulo12
+            // 
+            this.btnSubmodulo12.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnSubmodulo12.FlatAppearance.BorderSize = 0;
+            this.btnSubmodulo12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmodulo12.Location = new System.Drawing.Point(0, 24);
+            this.btnSubmodulo12.Name = "btnSubmodulo12";
+            this.btnSubmodulo12.Size = new System.Drawing.Size(127, 23);
+            this.btnSubmodulo12.TabIndex = 5;
+            this.btnSubmodulo12.Text = "Submodulo 2";
+            this.btnSubmodulo12.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmodulo11
+            // 
+            this.btnSubmodulo11.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnSubmodulo11.FlatAppearance.BorderSize = 0;
+            this.btnSubmodulo11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmodulo11.Location = new System.Drawing.Point(0, 1);
+            this.btnSubmodulo11.Name = "btnSubmodulo11";
+            this.btnSubmodulo11.Size = new System.Drawing.Size(127, 23);
+            this.btnSubmodulo11.TabIndex = 4;
+            this.btnSubmodulo11.Text = "Submodulo 1";
+            this.btnSubmodulo11.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel2.Controls.Add(this.btnSubmodulo21);
+            this.panel2.Location = new System.Drawing.Point(0, 267);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(127, 27);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnSubmodulo21
+            // 
+            this.btnSubmodulo21.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnSubmodulo21.FlatAppearance.BorderSize = 0;
+            this.btnSubmodulo21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmodulo21.Location = new System.Drawing.Point(0, 2);
+            this.btnSubmodulo21.Name = "btnSubmodulo21";
+            this.btnSubmodulo21.Size = new System.Drawing.Size(127, 23);
+            this.btnSubmodulo21.TabIndex = 7;
+            this.btnSubmodulo21.Text = "Submodulo 1";
+            this.btnSubmodulo21.UseVisualStyleBackColor = false;
             // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(770, 497);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnModulo2);
+            this.Controls.Add(this.btnModulo1);
             this.Name = "FrmMenu";
             this.Text = "Menú";
+            this.Load += new System.EventHandler(this.FrmMenu_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnModulo1;
+        private System.Windows.Forms.Button btnModulo2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSubmodulo13;
+        private System.Windows.Forms.Button btnSubmodulo12;
+        private System.Windows.Forms.Button btnSubmodulo11;
+        private System.Windows.Forms.Button btnSubmodulo21;
     }
 }
 
--- SystemRonda/SystemRonda/FrmMenu.cs
+++ SystemRonda/SystemRonda/FrmMenu.cs
@@ -16,5 +16,59 @@ namespace SystemRonda
         {
             InitializeComponent();
         }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            btnModulo1.Location = new Point(0, 127);
+            btnModulo2.Location = new Point(0, 164);
+            panel1.Hide();
+            panel2.Hide();
+        }
+
+        private void btnModulo1_Click(object sender, EventArgs e)
+        {
+            if (panel1.Visible)
+            {
+                panel1.Visible = false;
+            }
+            else
+            {
+                panel1.Visible = true;
+            }
+
+            btnModulo1.Location = new Point(0, 126);
+            panel1.Location = new Point(0, 160);
+            btnModulo2.Location = new Point(0, 233);
+            panel2.Hide();
+
+            if (!panel1.Visible)
+            {
+                btnModulo1.Location = new Point(0, 127);
+                btnModulo2.Location = new Point(0, 164);
+            }
+        }
+
+        private void btnModulo2_Click(object sender, EventArgs e)
+        {
+            if (panel2.Visible)
+            {
+                panel2.Visible = false;
+            }
+            else
+            {
+                panel2.Visible = true;
+            }
+
+            btnModulo1.Location = new Point(0, 127);
+            btnModulo2.Location = new Point(0, 164);
+            panel2.Location = new Point(0, 200);
+            panel1.Hide();
+
+            if (!panel2.Visible)
+            {
+                btnModulo1.Location = new Point(0, 127);
+                btnModulo2.Location = new Point(0, 164);
+            }
+        }
     }
 }
